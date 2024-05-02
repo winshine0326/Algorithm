d260a8db521a68d9eca013f9d8bb8c229a5c3322
winshine0326 <162104541+winshine0326@users.noreply.github.com>
2024-05-02 12:26:26 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(void){
+    int n;
+    int x;
+    int count=0;
+
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        int sum=0;
+        scanf("%d",&x);
+        for(int j=x-1;j>=1;j--){
+            if(j==1)
+                count++;
+            if(x%j==0) {
+                break;
+            }
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
