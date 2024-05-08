b1d9ac2a9d2cf2d453e5cba42e63bc598f2cff24
winshine0326 <162104541+winshine0326@users.noreply.github.com>
2024-05-08 15:27:56 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 148 ms, Memory: 213884 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -1,18 +1,17 @@
 #include<stdio.h>
 #include<string.h>
-
-
+int alpha[27][2000002];
 int main(void){
-    int alpha[27][2000]={};
-    char s[2002];
+    char s[2000002];
     int q;
     char c;
     int a,b;
 
     scanf("%s",s);
     alpha[s[0]-'a'][0]=1;
+    unsigned len=strlen(s);
 
-    for(int i=1;i<strlen(s);i++){
+    for(int i=1;i<len;i++){
         for(int j=0;j<27;j++){
             alpha[j][i]=alpha[j][i-1];
         }
@@ -24,12 +23,7 @@ int main(void){
         getchar();
         scanf("%c",&c);
         scanf("%d %d",&a,&b);
-        if(a==0) {
-            printf("%d\n", alpha[c - 'a'][b]);
-        }else{
-            printf("%d\n",alpha[c-'a'][b]-alpha[c-'a'][a-1]);
-        }
-
+        printf("%d\n",alpha[c-'a'][b]-alpha[c-'a'][a-1]);
     }
 
     return 0;
