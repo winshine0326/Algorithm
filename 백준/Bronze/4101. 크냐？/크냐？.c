f72002eaacdaa5ec16ed4810fe3d7e1abd7bc912
winshine0326 <162104541+winshine0326@users.noreply.github.com>
2024-05-18 22:56:27 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void){
+    int a,b;
+    while(1){
+        scanf("%d %d",&a,&b);
+        if(a==0&&b==0)
+            break;
+        printf("%s\n",a>b?"Yes":"No");
+    }
+    return 0;
+}
