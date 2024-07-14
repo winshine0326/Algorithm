1aa7e9b725e69bfc6c62d1a04c36296df36ec8d0
류승찬 <162104541+winshine0326@users.noreply.github.com>
2024-07-14 20:50:24 +0900

    [Bronze V] Title: Serca, Time: 84 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void){
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        printf(" @@@   @@@ \n"
+               "@   @ @   @\n"
+               "@    @    @\n"
+               "@         @\n"
+               " @       @ \n"
+               "  @     @  \n"
+               "   @   @   \n"
+               "    @ @    \n"
+               "     @     \n");
+    }
+}
