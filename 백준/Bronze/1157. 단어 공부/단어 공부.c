e9fdbb6facd1ba712916f4351a34ca5402026ff3
류승찬 <162104541+winshine0326@users.noreply.github.com>
2024-09-25 00:21:33 +0900

    [Bronze I] Title: 단어 공부, Time: 8 ms, Memory: 1972 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.c
+++ 백준/Bronze/1157. 단어 공부/단어 공부.c
@@ -1,44 +1,36 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
-
 int main() {
-    char s[1000001];
-    int alpha[26] = {0};
-    int len, flag = 0;
-
-    // 입력 받기
-    scanf("%s", s);
-    len = strlen(s);
-
-    // 알파벳 빈도수 계산
-    for (int i = 0; i < len; i++) {
-        char temp = tolower(s[i]);  // 소문자로 변환
-        if (temp >= 'a' && temp <= 'z') {
-            alpha[temp - 'a']++;
-        }
-    }
-
-    // 가장 많이 등장한 알파벳 찾기
-    int max = 0;
-    char max_alpha = '?';
-
-    for (int i = 0; i < 26; i++) {
-        if (alpha[i] > max) {
-            max = alpha[i];
-            max_alpha = 'A' + i;  // 대문자로 변환
-            flag = 0;
-        } else if (alpha[i] == max) {
-            flag = 1;
-        }
-    }
-
-    // 결과 출력
-    if (flag == 1) {
-        printf("?\n");
-    } else {
-        printf("%c\n", max_alpha);
-    }
-
-    return 0;
+	char s[1000001];
+	int alpha[26]={0,};
+	int len,flag;
+	char temp;
+	
+	scanf("%s",s);
+	len = strlen(s);
+	for(int i=0;i<len;i++){
+		if(s[i]>=65&&s[i]<97)
+			temp = s[i]+32;
+		else
+			temp = s[i];
+		alpha[temp-'a']++;
+	}	
+	int max = 0;
+	char max_alpha = '?';
+	for(int i=0;i<26;i++){
+		if(max<alpha[i]){
+			max = alpha[i];
+			max_alpha = 'A'+i;
+			flag = 0;
+		}
+		else if(max==alpha[i]){
+			flag = 1;
+		}
+	}
+	if (flag==1)
+		printf("?");
+	else
+		printf("%c",max_alpha);
+	
+	return 0;
 }
