aae128de8e59341412390be7d4c4418ac335b5dd
류승찬 <162104541+winshine0326@users.noreply.github.com>
2024-12-02 23:13:21 +0900

    [Bronze IV] Title: 가희와 클럽 오디션 1, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30794. 가희와 클럽 오디션 1/가희와 클럽 오디션 1.py
@@ -0,0 +1,11 @@
+n,s = input().split()
+if s == "miss":
+    print(0)
+elif s=="bad":
+    print(int(n)*200)
+elif s=="cool":
+    print(int(n)*400)
+elif s=="great":
+    print(int(n)*600)
+elif s=="perfect":
+    print(int(n)*1000)
