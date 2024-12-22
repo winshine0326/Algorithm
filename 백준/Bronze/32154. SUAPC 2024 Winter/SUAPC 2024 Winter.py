53e69d31cb18a380513520f2aa767f85eaaa18fe
류승찬 <162104541+winshine0326@users.noreply.github.com>
2024-12-22 23:28:42 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -0,0 +1,32 @@
+s = int(input())
+
+if s == 1 : 
+    print(11)
+    print("A B C D E F G H J L M")
+elif s == 2 :
+    print(9)
+    print("A C E F G H I L M")
+elif s == 3 :
+    print(9)
+    print("A C E F G H I L M")
+elif s == 4 :
+    print(9)
+    print("A B C E F G H L M")
+elif s == 5 :
+    print(8)
+    print("A C E F G H L M")
+elif s == 6 :
+    print(8)
+    print("A C E F G H L M")
+elif s == 7 :
+    print(8)
+    print("A C E F G H L M")
+elif s == 8 :
+    print(8)
+    print("A C E F G H L M")
+elif s == 9 :
+    print(8)
+    print("A C E F G H L M")
+elif s == 10 :
+    print(8)
+    print("A B C F G H L M")
