73e1f2a2dc9e9e2bf64f48b7c73a575187dbf749
류승찬 <162104541+winshine0326@users.noreply.github.com>
2025-05-17 23:07:28 +0900

    [Bronze III] Title: 연길이의 이상형, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/20540. 연길이의 이상형/연길이의 이상형.py
@@ -0,0 +1,17 @@
+s = input()
+if s[0] == 'I':
+    print('E',end='')
+else:
+    print('I',end='')
+if s[1] == 'N':
+    print('S',end='')
+else:
+    print('N',end='')
+if s[2] == 'T':
+    print('F',end='')
+else:
+    print('T',end='')
+if s[3] == 'J':
+    print('P',end='')
+else:
+    print('J',end='')
