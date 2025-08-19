287378761c6ee64b19794e49400cc285c7c0504f
류승찬 <162104541+winshine0326@users.noreply.github.com>
2025-08-19 22:43:51 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,8 @@
+select t.item_id, i.item_name, i.rarity from item_info as i
+join item_tree as t on i.item_id = t.item_id
+where t.parent_item_id in (
+    select item_id
+    from item_info
+    where rarity = "RARE"
+)
+order by t.item_id desc;
